bba23dd5af4da203014b5b66c20e4cc5641c10be
Sébastien Pezac <sebastien.pezac@imt-atlantique.net>
2021-03-08 18:15:45 +0100

    Add hostname parameter

--- src/api/api.py
+++ src/api/api.py
@@ -11,9 +11,10 @@ async def rootGet():
 
 
 @router.post("/")
-async def sendFioResult(file: UploadFile = File(...)):
+async def sendFioResult(file: UploadFile = File(...), hostname: str = "Unknown"):
     """
     WIP - Post a FIO json output file to the system and save it to the database
+    :param hostname: server/computer hostname
     :param file: FIO json output file
     :return: boolean
     """
@@ -21,7 +22,9 @@ async def sendFioResult(file: UploadFile = File(...)):
     # TODO verify the file integrity (type is json, fio fields are presents)
     contents = await file.read()
     contents = importers.removePointInJsonKeys(contents)
-    insertInMongo(importers.jsonfileToDic(contents))
+    contents = importers.jsonfileToDic(contents)
+    contents["hostname"] = hostname
+    insertInMongo(contents)
     return True
 
 
@@ -31,7 +34,7 @@ async def getResultList():
     for r in getAllResults():
         current_result = {
             "id": r["id"],
-            "hostname": "TODO",  # TODO add hostname
+            "hostname": r["hostname"],
             "time": r["time"],
             "timestamp": r["timestamp"],
             "jobs": []
@@ -51,5 +54,5 @@ async def getResultList():
 
 
 @router.get("/result/{result_id}")
-async def getResultList(result_id):
+async def getResultList(result_id: str):
     return getResultById(result_id)
