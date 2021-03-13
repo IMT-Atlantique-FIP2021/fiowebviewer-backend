f969a89aca4eba4ac81ceb78c9c59d270272c9c9
Sébastien Pezac <sebastien.pezac@imt-atlantique.net>
2021-03-14 00:28:12 +0100

    Improving error handling

--- src/api/result.py
+++ src/api/result.py
@@ -3,27 +3,38 @@ from typing import List, Union
 from bson import ObjectId
 from bson.errors import InvalidId
 from fastapi import UploadFile, File, APIRouter, Response, status
+from pydantic.error_wrappers import ValidationError
+
 from src.database.mongo import insertInMongo, getAllResults, getResultById, ResultNotFound
 from src.models.resultsListModel import ShortenResult
 from src.models.resultModel import FioResult
 
-router = APIRouter(prefix="/result", tags=["Results", "results", "Result", "result"])
+router = APIRouter(prefix="/result", tags=["Results"])
 
 
-@router.post("/post", status_code=status.HTTP_201_CREATED)
-async def send_fio_result(file: UploadFile = File(...), hostname: str = "Unknown"):
+@router.post("/post", status_code=status.HTTP_201_CREATED,
+             response_model=str,
+             responses={status.HTTP_422_UNPROCESSABLE_ENTITY: {"model": str}})
+async def send_fio_result(response: Response,
+                          file: UploadFile = File(...),
+                          hostname: str = "Unknown"):
     """
-    Post a FIO json output file to the system and save it into the database
+    Post a FIO json output file to the system and save it into the database.
+    Return the id of the new element.
 
+    :param response: Response
     :param hostname: server/computer hostname
     :param file: FIO json output file
-    :return: boolean
+    :return: str
     """
-    # TODO handle error
-    json_string = await file.read()
-    contents = FioResult.parse_raw(json_string)
-    contents.hostname = hostname
-    return insertInMongo(contents)
+    try:
+        json_string = await file.read()
+        contents = FioResult.parse_raw(json_string)
+        contents.hostname = hostname
+        return insertInMongo(contents)
+    except ValidationError:
+        response.status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
+        return f"{file.filename} is not an valid json fio result"
 
 
 @router.get("/list", response_model=List[ShortenResult])
@@ -41,10 +52,10 @@ async def get_results_list(limit: int = 0) -> List[ShortenResult]:
 
 
 @router.get("/byId/{result_id}", response_model=FioResult,
-            responses={status.HTTP_404_NOT_FOUND: {"model": None},
+            responses={status.HTTP_404_NOT_FOUND: {"model": str},
                        status.HTTP_422_UNPROCESSABLE_ENTITY: {"model": None}},
             status_code=status.HTTP_200_OK)
-async def get_a_result(result_id: str, response: Response) -> Union[FioResult, None]:
+async def get_a_result(result_id: str, response: Response) -> Union[FioResult, str, None]:
     """
     Fetch the FioResult matching the result_id if it is founded
 
@@ -53,8 +64,8 @@ async def get_a_result(result_id: str, response: Response) -> Union[FioResult, N
     :return: Union[FioResult, None]
     """
     try:
-        result_id = ObjectId(result_id)
-        result = getResultById(result_id)
+        result_object_id = ObjectId(result_id)
+        result = getResultById(result_object_id)
         if type(result) is FioResult:
             return result
     except ResultNotFound:
@@ -62,5 +73,6 @@ async def get_a_result(result_id: str, response: Response) -> Union[FioResult, N
         return None
     except InvalidId:
         response.status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
-        return None
+        return None  # f"The id {result_id} is not a valid id."
+        # I don't now why but it try to check the return with pydantic, spoiler: it doesn't work
     raise Exception("Unknown error")
