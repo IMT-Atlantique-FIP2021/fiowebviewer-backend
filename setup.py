156ad796f7f193c618fac9d6e74c66d39ecaa32a
sebastienHUGDELARAUZE <sebastien.hugdelarauze@gmail.com>
2021-03-15 18:28:52 +0100

    Fixed pre-commit

--- setup.py
+++ setup.py
@@ -35,22 +35,22 @@ def get_install_requires():
 setup(
     name="fiowebviewer-backend",
     version=__version__,
-      description="Backend part of fiowebviewer : Webapp for visualising fio results",
-      author="Sebastien Pezac",
-      author_email="sebastien.pezac@imt-atlantique.net",
-      packages=[
-          "src",
-          "src.api",
-          "src.database",
-          "src.tools",
-      ],
-      install_requires=get_install_requires(),
-      package_data={
-          "src": [
-              "database.json",
-          ],
-          "src.static": [
-              "resultList.json",
-          ],
-      },
-    )
+    description="Backend part of fiowebviewer : Webapp for visualising fio results",
+    author="Sebastien Pezac",
+    author_email="sebastien.pezac@imt-atlantique.net",
+    packages=[
+        "src",
+        "src.api",
+        "src.database",
+        "src.tools",
+    ],
+    install_requires=get_install_requires(),
+    package_data={
+        "src": [
+            "database.json",
+        ],
+        "src.static": [
+            "resultList.json",
+        ],
+    },
+)
